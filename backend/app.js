817cb3e7b881f14d904c7330de1b628ce41e7a6a
choonsiang <choonsiang_ccs@hotmail.com>
2023-05-22 16:46:04 +0800

    included cors in backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./config/db";
+import cors from 'cors'
 // const bodyParser = require("body-parser")
 
 //routes
@@ -12,6 +13,7 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req,res)=>{
