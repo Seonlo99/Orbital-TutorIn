3171928b6a0eef64796d4e04a77c3fd60a374631
choonsiang <choonsiang_ccs@hotmail.com>
2023-06-10 17:21:23 +0800

    Fixed sort dropdown not showing for smaller device

--- frontend/src/components/Filters/Sort.jsx
+++ frontend/src/components/Filters/Sort.jsx
@@ -17,7 +17,7 @@ export const Sort = ({sortBy="", setSortByHandler}) => {
     <li className='relative group flex flex-row items-center hover:cursor-pointer'>
         <div>Sort</div>
         <MdKeyboardArrowDown />
-        <div className='hidden transition-all duration-500 absolute bottom-0 left-0 transform translate-y-full lg:group-hover:block w-max'>
+        <div className='hidden transition-all duration-500 absolute bottom-0 left-0 transform translate-y-full group-hover:block w-max'>
             <ul className='flex flex-col shadow-lg rounded-lg divide-y overflow-hidden bg-white'>
                 <button onClick={handleSort} value="New" className={`hover:bg-gray-300 hover:text-black py-1 px-1 ${sortBy==="New" && 'bg-blue-500 text-white'}`}>Newest</button>
                 <button onClick={handleSort} value="Upvote" className={`hover:bg-gray-300 hover:text-black py-1 px-1 ${sortBy==="Upvote" && 'bg-blue-500 text-white'}`}>Most Upvotes</button>
