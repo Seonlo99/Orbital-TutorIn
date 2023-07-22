7703843b0afca09f58c8435ad3ebeb43a76387b5
choonsiang <choonsiang_ccs@hotmail.com>
2023-07-22 23:32:52 +0800

    added more test.

--- frontend/cypress/e2e/User.cy.js
+++ frontend/cypress/e2e/User.cy.js
@@ -13,6 +13,16 @@ describe('User Registration and Login', () => {
     cy.findByText(/cypress-test/i)
   })
 
+  it('Login with invalid credential shows error', () => {
+    cy.visit('/')
+    cy.contains('Sign In')
+    cy.findByRole('button', {  name: /sign in/i}).click()
+    cy.findByRole('textbox', {name: /username:/i}).type("cypress-test")
+    cy.findByLabelText(/password:/i).type("wrongpassword")
+    cy.findByRole('button', {name: /submit/i}).click()
+    cy.findByText(/User does not exist or invalid password/i)
+  })
+
   it('Register Success', () => {
     cy.visit('/')
     cy.contains('Sign In')
@@ -27,5 +37,20 @@ describe('User Registration and Login', () => {
     cy.findByTestId("RegisterCfmPassword").type(name)
     cy.findByRole('button', {name: /submit/i}).click()
     cy.findByText(name)
+  })
+
+  it('Registering with existing account shows error', () => {
+    cy.visit('/')
+    cy.contains('Sign In')
+    cy.findByRole('button', {  name: /sign in/i}).click()
+    cy.findByText(/register now/i).click()
+    let name = "cypress-test"
+    cy.findByPlaceholderText(/Enter Username/i).type(name)
+    cy.findByPlaceholderText(/Enter Name/i).type(name)
+    cy.findByPlaceholderText(/Enter email/i).type(name+"@email.com")
+    cy.findByTestId("RegisterPassword").type(name)
+    cy.findByTestId("RegisterCfmPassword").type(name)
+    cy.findByRole('button', {name: /submit/i}).click()
+    cy.findByText("User have already registered")
   })
 })
--- frontend/cypress/e2e/forum.cy.js
+++ frontend/cypress/e2e/forum.cy.js
@@ -82,6 +82,116 @@ describe('Forum', () => {
         cy.contains(/post deleted/i)
     })
 
+    it('Filter post by latest', () => {
+        cy.findByText(/cypress-test/i)
+        cy.findByRole('button', {name: /forum/i}).click()
+  
+        cy.findByRole('button', {name: /new/i}).click()
+        cy.contains(/new post/i)
+        cy.findByRole("textbox").type("This is a test post by cypress")
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div').type("test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(4) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)').type("CS2100{enter}")
+        cy.findByRole('button', {name: /post/i}).click({force:true})
+
+        cy.contains(/post created/i)
+
+        cy.findByRole("button", {name: /forum/i}).click()
+
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(2) > div:nth-child(1)').contains("This is a test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(2) > div:nth-child(1)').findByText("This is a test post by cypress").click()
+        cy.findByRole('button', {name: /delete/i}).click()
+
+        cy.contains(/post deleted/i)
+    })
+
+    it('Filter post by searching for post title', () => {
+        cy.findByText(/cypress-test/i)
+        cy.findByRole('button', {name: /forum/i}).click()
+  
+        cy.findByRole('button', {name: /new/i}).click()
+        cy.contains(/new post/i)
+        cy.findByRole("textbox").type("This is a test post by cypress")
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div').type("test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(4) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)').type("CS2100{enter}")
+        cy.findByRole('button', {name: /post/i}).click({force:true})
+
+        cy.contains(/post created/i)
+
+        cy.findByRole("button", {name: /forum/i}).click()
+
+        cy.findByRole('textbox').type("This is a test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(2) > div:nth-child(1)').contains("This is a test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(2) > div:nth-child(1)').findByText("This is a test post by cypress").click()
+        cy.findByRole('button', {name: /delete/i}).click()
+
+        cy.contains(/post deleted/i)
+    })
+
+    it('Filter post by searching for non existent post', () => {
+        cy.findByText(/cypress-test/i)
+        cy.findByRole('button', {name: /forum/i}).click()
+
+        cy.findByRole('textbox').type("There should be no such post by cypress and return error 000")
+
+        cy.contains(/Created by/i).should('not.exist')
+    })
+
+    it('Filter post by tag CS2100, should show post with tag CS2100', () => {
+        cy.findByText(/cypress-test/i)
+        cy.findByRole('button', {name: /forum/i}).click()
+  
+        cy.findByRole('button', {name: /new/i}).click()
+        cy.contains(/new post/i)
+        cy.findByRole("textbox").type("This is a test post by cypress")
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div').type("test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(4) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)').type("CS2100{enter}")
+        cy.findByRole('button', {name: /post/i}).click({force:true})
+
+        cy.contains(/post created/i)
+
+        cy.findByRole("button", {name: /forum/i}).click()
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)').type("CS2100{enter}")
+
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(2) > div:nth-child(1)').contains("This is a test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(2) > div:nth-child(1)').findByText("This is a test post by cypress").click({force:true})
+        cy.findByRole('button', {name: /delete/i}).click()
+
+        cy.contains(/post deleted/i)
+    })
+
+    it('Filter post by tag CS2105, should show post with tag CS2100', () => {
+        cy.findByText(/cypress-test/i)
+        cy.findByRole('button', {name: /forum/i}).click()
+  
+        cy.findByRole('button', {name: /new/i}).click()
+        cy.contains(/new post/i)
+        cy.findByRole("textbox").type("This is a test post by cypress")
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div').type("test post by cypress")
+
+        cy.get('#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(4) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)').type("CS2100{enter}")
+        cy.findByRole('button', {name: /post/i}).click({force:true})
+
+        cy.contains(/post created/i)
+
+        cy.findByRole("button", {name: /forum/i}).click()
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)').type("CS2105{enter}")
+
+        cy.get("This is a test post by cypress").should("not.exist")
+
+        cy.findByRole("button", {name: /home/i}).click()
+        cy.findByRole("button", {name: /forum/i}).click()
+        cy.get('#root > div > div:nth-child(1) > div > section:nth-child(2) > div:nth-child(1)').findByText("This is a test post by cypress").click()
+        cy.findByRole('button', {name: /delete/i}).click()
+
+        cy.contains(/post deleted/i)
+    })
 
 
 })
